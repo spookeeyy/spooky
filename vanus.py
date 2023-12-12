fe9c3c2eee711c834a7f332da10b333dbc094509
spookeeyy <152895833+spookeeyy@users.noreply.github.com>
2023-12-12 11:33:00 +0200

    more complicated
    22232323

---
+++ vanus.py
@@ -0,0 +1,8 @@
+nimi = input("Palun ütle oma nimi: ")
+print(type(nimi))
+print("Tere, " + nimi + "!")
+
+vanus = int(input("Palun sisesta oma vanus: "))
+vanus_viie_aasta_p2rast = vanus + 5
+print(type(vanus))
+print("Teie vanus on: " + str(vanus_viie_aasta_p2rast))
